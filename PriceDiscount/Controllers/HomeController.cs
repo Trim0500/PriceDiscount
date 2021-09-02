52554b8945c41b580b14995e03c914f813d18674
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-09-02 08:49:29 -0400

    Added in comments

--- PriceDiscount/Controllers/HomeController.cs
+++ PriceDiscount/Controllers/HomeController.cs
@@ -9,9 +9,10 @@ namespace PriceDiscount.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
+        [HttpGet] //Use a tag to mapp the call to the get protocol
         public IActionResult Index()
         {
+            //Set all the fields for the index to 0
             ViewBag.subTotal = 0;
             ViewBag.discountPercent = 0;
             ViewBag.discountTotal = 0;
@@ -19,15 +20,15 @@ namespace PriceDiscount.Controllers
             return View();
         }
 
-        [HttpPost]
+        [HttpPost] //Use a tag top map the submit button post call to this function
         public IActionResult Index(Price model)
         {
-            if(ModelState.IsValid)
+            if(ModelState.IsValid) //Check if the model data is valid
             {
-                ViewBag.discountTotal = model.calculateDiscountTotal();
-                ViewBag.grandTotal = model.calculateGrandTotal();
+                ViewBag.discountTotal = model.calculateDiscountTotal(); //Set the discount toal to the model class' function
+                ViewBag.grandTotal = model.calculateGrandTotal(); //Set the grand total to the model class' function
             }
-            else
+            else //If the model is invalid, set everything to 0
             {
                 ViewBag.discountTotal = 0;
                 ViewBag.grandTotal = 0;
--- PriceDiscount/Model/Price.cs
+++ PriceDiscount/Model/Price.cs
@@ -6,16 +6,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PriceDiscount.Model
 {
+    //Model class for the data inoutted and calculated
     public class Price
     {
-        [Required(ErrorMessage = "You must enter a total")]
-        [Range(0, 1000000, ErrorMessage = "You must enter a value within the range of 0 to 1,000,000")]
+        [Required(ErrorMessage = "You must enter a total")] //Use the tag helpers from the view imports to declare a required field with a custom message
+        [Range(0, 1000000, ErrorMessage = "You must enter a value within the range of 0 to 1,000,000")] //Use the tag helpers from the view imports to declare a range for the field with a custom message
         public double? subTotal { get; set; }
         [Required(ErrorMessage = "You must enter a discount")]
         [Range(0, 75, ErrorMessage = "You must enter a value between 0 and 75%")]
         public double? discountPercent { get; set; }
         public double? discountTotal { get; set; }
         public double? grandTotal { get; set; }
+
+        //Declare a method to calculate the discount total
         public double? calculateDiscountTotal()
         {
             double total;
@@ -27,6 +30,7 @@ namespace PriceDiscount.Model
             return total;
         }
 
+        //Declare a function to calculate the grand total
         public double? calculateGrandTotal()
         {
             double discount;
